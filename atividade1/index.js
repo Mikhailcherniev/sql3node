8463d2d1c5bfe98916fc4d9dbb797b7371464e6d
Micael_Henrique <133661765+Mikhailcherniev@users.noreply.github.com>
2024-06-05 14:06:59 -0300

    Initial commit

---
+++ atividade1/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./database');
+
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  db.all("SELECT * FROM products", [], (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.render('index', { products: rows });
+    }
+  });
+});
+
+app.post('/products', (req, res) => {
+  const { name, price, stock } = req.body;
+  db.run("INSERT INTO products (name, price, stock) VALUES (?, ?, ?)", [name, price, stock], (err) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.post('/delete', (req, res) => {
+  db.get("SELECT id FROM products ORDER BY id ASC LIMIT 1", [], (err, row) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (row) {
+      db.run("DELETE FROM products WHERE id = ?", [row.id], (err) => {
+        if (err) {
+          res.status(500).send(err.message);
+        } else {
+          res.redirect('/');
+        }
+      });
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
